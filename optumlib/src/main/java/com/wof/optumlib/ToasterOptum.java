62f83ab2e9926ffda9941171c15b9f5e7bb08c0f
Vindhya <you@example.comvindhya.singh@ezeiatech.in>
2022-10-27 09:16:28 +0530

    Initial commit

---
+++ optumlib/src/main/java/com/wof/optumlib/ToasterOptum.java
@@ -0,0 +1,11 @@
+package com.wof.optumlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterOptum {
+    public static void showToast(Context context){
+        Toast.makeText(context, "Hello Toast", Toast.LENGTH_LONG).show();
+
+    }
+}
